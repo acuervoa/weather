8f5885164494a9222ea7cbba67cd76fd0c4aa5eb
andres.cuervo <andres.cuervo@tecnilogica.com>
2016-05-30 12:54:56 +0200

    prettify code

--- scripts.js
+++ scripts.js
@@ -1,43 +1,40 @@
-$(document).ready(function(){
-	
-	var getIPInfo = function(){
-		var ipInfoAPI = "http://ipinfo.io/json";
-		$.getJSON(ipInfoAPI + "?callback=?", function(json){
-			apiWeatherURL(json.city);	
-		});
-	};
+$(document).ready(function() {
+
+    var getIPInfo = function() {
+        var ipInfoAPI = "http://ipinfo.io/json";
+        $.getJSON(ipInfoAPI + "?callback=?", function(json) {
+            apiWeatherURL(json.city);
+        });
+    };
 
 
-	var apiWeatherURL = function(city){
-		var APIKEY = "bb98943838568d5a03db08a7fc567d68";
-			var units = "metric";
-			var language = "es";
-			//var city = json.city;
+    var apiWeatherURL = function(city) {
+        var APIKEY = "bb98943838568d5a03db08a7fc567d68";
+        var units = "metric";
+        var language = "es";
+        //var city = json.city;
 
-			var apiWeatherURL = "http://api.openweathermap.org/data/2.5/weather?q=" + 	city 
-								+ "&units=" + units 
-								+ "&lang=" +  language 
-								+ "&APPID=" + APIKEY; 
-			
+        var apiWeatherURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=" + units + "&lang=" + language + "&APPID=" + APIKEY;
 
-			$.ajax({
-				url: apiWeatherURL,
-				success: function(json){
-					
-					$('#city').html(json.name + ", " + json.sys.country);
-					$('#weather').html(json.weather[0].description);
-					$('#temperature').html(json.main.temp);
-					$('#humidity').html(json.main.humidity);
-					$('#pressure').html(json.main.pressure);
-					$('#wind').html(json.wind.speed);
-					$(".image").attr("src","http://openweathermap.org/img/w/" + json.weather[0].icon +".png");
-					//changeBackgroundImage(json.weather[0].description);
-				}	
-			});
-	
-	};
 
-	/*var changeBackgroundImage = function(weather) {
+        $.ajax({
+            url: apiWeatherURL,
+            success: function(json) {
+
+                $('#city').html(json.name + ", " + json.sys.country);
+                $('#weather').html(json.weather[0].description);
+                $('#temperature').html(json.main.temp);
+                $('#humidity').html(json.main.humidity);
+                $('#pressure').html(json.main.pressure);
+                $('#wind').html(json.wind.speed);
+                $(".image").attr("src", "https://openweathermap.org/img/w/" + json.weather[0].icon + ".png");
+                //changeBackgroundImage(json.weather[0].description);
+            }
+        });
+
+    };
+
+    /*var changeBackgroundImage = function(weather) {
 		  var flickerAPI = "http://api.flickr.com/services/flickr.photos.search";
   			$.getJSON( flickerAPI, {
   				api_key: "9e9aefe2303bbcce7953c3fa1f4b2c68",
@@ -63,15 +60,8 @@ $(document).ready(function(){
     	
 	};*/
 
-	getIPInfo();
+    getIPInfo();
 
-	//console.log(getIPInfo());
-	//changeBackgroundImage('calm');
+    //console.log(getIPInfo());
+    //changeBackgroundImage('calm');
 });
-
-
-
-
-
-
-
